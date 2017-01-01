e9457d2eda03ff207774f90ea05bb02aefd19cd7
Danny Currie <danny.currie42@gmail.com>
2017-01-01 19:52:37 +0000

    http service and test route added

--- Synth/client/app/app.component.ts
+++ Synth/client/app/app.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import { Http, Response  } from '@angular/http';
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  
+  name = 'Danny'; 
+
+  constructor (private http: Http) {}
+
+  ngOnInit(){
+    console.log("attempting to call server");
+    this.http.get('app/Test');
+  }
+}
+
+
